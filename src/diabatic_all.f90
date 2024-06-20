e7fbdccf40f7b1a23fb42c5b568063ca865539f3
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2024-06-20 20:08:44 +0900

    FEAT : diabatic_all.f90 is slightly modified
    Some statements are written in diabatic_all.f90
    This is not a important change and this version can be ignored

---
+++ src/diabatic_all.f90
@@ -0,0 +1,22 @@
+module diabatic_all
+
+    use parameter, only : rkappa, cp
+    use biseki, only : biseki_biseki
+
+    implicit none
+
+    private
+    public :: 
+
+    contains
+
+
+    !function Exner(Numerator, Denominator) result(output)
+    !    real(4), intent(in) :: Numerator
+    !    real(4), intent(in) :: Denominator
+    !    real(4) :: output
+
+    !    output = 
+
+
+end module diabatic_all
