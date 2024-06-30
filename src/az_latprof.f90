cfd00737e5c5095eba02189074e231e6e0c9056e
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2024-06-30 22:45:50 +0900

    FEAT : New module az_latprof is made
    az_latprof.f90 is made to compute the meridional profile of A_Z.
    In this program, unnecessary components for global mean A_Z are
    not included in the output A_Z.
    Only necessary terms up to the fifth order in the Taylor series
    expansion are considered.
    This module has NOT BEEN DEBUGGED in this commit.
    
    Makefile.inc is also modified to compile az_latprof.
    
    Some namelists are also added in this commit, but they are not
    related to az_latprof.

--- src/az_latprof.f90
+++ src/az_latprof.f90
@@ -1,13 +1,14 @@
 module az_latprof
 
     use parameter, only : rkappa, grav, cp
-    use parameter, only : pai -> pi
+    !use parameter, only : pai
     use com_var  , only : jm, ko, alat, pout
+    use mim_var  , only : pd_pdd
 
     implicit none
 
     private
-    public ::
+    public :: az_latprof_vint, energy_az_highPrecision
 
     contains
 
@@ -17,14 +18,14 @@ module az_latprof
     ! Note:
     !   It is not used for global mean A_Z (because of the accuracy)
     !
-    subroutine energy_az_vint( p_pds, p_pdds, pd_pdd, pt_pdds, pt_ym, &
+    subroutine az_latprof_vint( p_pds, p_pdds, pd_pdd, pt_pdds, pt_ym, &
          &                     az_zm_vint )
       use parameter, only : rkappa, grav, cp, pai
       use com_var, only : jm, ko, alat, pout
       implicit none
       real(4),intent(in)  :: p_pds(jm)
       real(4),intent(in)  :: p_pdds(1)
-      real(4),intent(in)  :: pd_pdd(jm, ko)         ! p+ at 
+      real(4),intent(in)  :: pd_pdd(jm, ko)         ! p+ at p++ surfaces
       real(4),intent(in)  :: pt_pdds(1)             ! Potential Temperature at the surface p++ level
       real(4),intent(in)  :: pt_ym(ko)              ! Potential Temperature at each p++ level
       real(4),intent(out) :: az_zm_vint(jm)
@@ -35,6 +36,8 @@ module az_latprof
       real(4) :: const
       real(4) :: az_modify(jm)
 
+      write(*,*) 'Marker1'
+
       ! pd_ym : global mean p+ at the p++ levels
       !         pd_ym must be almost equal to standard p++ levels 
       !         except under the ground.
@@ -42,13 +45,8 @@ module az_latprof
            &                    pd_ym )
       
       ! get integrand
-      const = cp * (1.0e+5)**(-rkappa) / (1+rkappa) / grav
-      do k=1, ko
-         do j=1, jm
-            integ(j,k) = const * ( ( pd_pdd(j,k)*100 )**(1+rkappa) &
-                 &                - ( pd_ym(k)*100)**(1+rkappa) )
-         end do
-      end do
+      call energy_az_highPrecision(pd_ym(1:ko)   , &  !! IN
+                                 & integ(1:jm,1:ko))  !! OUT
 
       ! integrate with pt
       call integral_pt_ym( jm, ko, pout, p_pdds, pt_ym, pt_pdds, integ, &
@@ -61,7 +59,64 @@ module az_latprof
 
       az_zm_vint(:) = az_zm_vint(:) + az_modify(:)
 
-    end subroutine energy_az_vint
+    end subroutine az_latprof_vint
+
+
+    subroutine energy_az_simple(pd_ym, integrand)
+        real(4), intent(in)  :: pd_ym(ko)
+        real(4), intent(out) :: integrand(jm,ko)
+
+        real(4) :: expansion(jm)                      !! p_dagger - p_dagger_dagger
+
+        integer, parameter :: coeff = cp / (100000._4**rkappa * (1._4 + rkappa) * grav)
+        integer :: k
+
+        do k = 1, ko
+            expansion(1:jm) = (pd_pdd(1:jm,k) - pd_ym(k)) / pd_ym(k)
+            integrand(1:jm,k)  &
+            & = coeff*(pd_ym(k)*100._4)**(1+rkappa) * (                                                              &  !! coefficient
+            &   (rkappa+1._4)*rkappa/2._4                                                                            &  !! 2st term
+            &                       *expansion(1:jm)*expansion(1:jm)                                                 &
+            & + (rkappa+1._4)*rkappa*(rkappa-1._4)/6._4                                                              &  !! 3nd term
+            &                       *expansion(1:jm)*expansion(1:jm)*expansion(1:jm)                                 &
+            & + (rkappa+1._4)*rkappa*(rkappa-1._4)*(rkappa-2._4)/24._4                                               &  !! 4rd term
+            &                       *expansion(1:jm)*expansion(1:jm)*expansion(1:jm)*expansion(1:jm)                 &
+            & + (rkappa+1._4)*rkappa+(rkappa-1._4)*(rkappa-2._4)*(rkappa-3._4)/120._4                                &  !! 5th term
+            &                       *expansion(1:jm)*expansion(1:jm)*expansion(1:jm)*expansion(1:jm)*expansion(1:jm) &
+            & )
+        enddo
+
+    end subroutine energy_az_simple
+
+
+    subroutine energy_az_highPrecision(pd_ym, integrand)
+        real(4), intent(in)  :: pd_ym(ko)
+        real(4), intent(out) :: integrand(jm,ko)
+
+        real(4) :: ratio(jm)
+
+        integer, parameter :: coeff = cp / (100000._4**rkappa * (1._4 + rkappa) * grav)
+        integer, parameter :: C2 = (rkappa+1)*rkappa/2._4
+        integer, parameter :: C3 = (rkappa+1)*rkappa*(rkappa-1)/6._4
+        integer, parameter :: C4 = (rkappa+1)*rkappa*(rkappa-1)*(rkappa-2)/24._4
+        integer, parameter :: C5 = (rkappa+1)*rkappa*(rkappa-1)*(rkappa-2)*(rkappa-3)/120._4
+        integer :: k
+
+        do k = 1, ko
+
+            ratio(1:jm) = pd_pdd(1:jm,k) / pd_ym(k)
+
+            integrand(1:jm,k) = C5 * ratio(1:jm)**5
+            integrand(1:jm,k) = integrand(1:jm,k) + ( C4 - 5*C5                ) * ratio(1:jm)**4
+            integrand(1:jm,k) = integrand(1:jm,k) + ( C3 - 4*C4 + 10*C5        ) * ratio(1:jm)**3
+            integrand(1:jm,k) = integrand(1:jm,k) + ( C2 - 3*C3 +  6*C4 - 10*C5) * ratio(1:jm)**2
+            integrand(1:jm,k) = integrand(1:jm,k) + (-C2 + 2*C3 -  3*C4 +  4*C5) * ratio(1:jm)
+            
+            integrand(1:jm,k) = integrand(1:jm,k) * coeff * (pd_ym(k)*100._4)**(1+rkappa)
+
+        enddo
+
+    end subroutine energy_az_highPrecision
 
 
 end module az_latprof
